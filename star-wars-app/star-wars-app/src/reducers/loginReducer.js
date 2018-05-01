49063ef635a5921d4f43332e993594029f72374c
Jatin Gupta <jatin92.cse@gmail.com>
2018-05-01 13:04:47 +0530

    added rootReducer

--- star-wars-app/star-wars-app/src/reducers/loginReducer.js
+++ star-wars-app/star-wars-app/src/reducers/loginReducer.js
@@ -1,6 +1,6 @@
 import {LOGIN_USER, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR} from '../constants/constants';
 
-let loginReducer = (state, action) => {
+let loginReducer = (state = [], action) => {
     switch(action.type){
         case LOGIN_USER: 
         case LOGIN_USER_SUCCESS:
